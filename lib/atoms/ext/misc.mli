de52e0831d10fa6892dd4e4bf33a063bb9d11bbe
xvw <xavier.vdw@gmail.com>
2026-01-18 18:14:35 +0100

    Add Option Module

--- lib/atoms/ext/misc.mli
+++ lib/atoms/ext/misc.mli
@@ -1,7 +1,18 @@
 (** Somn random function (notably for validation). *)
 
+(** [in_str_enum ?case_sensitive xs x] checks if [x] is include in [xs]. *)
 val in_str_enum : ?case_sensitive:bool -> string list -> string -> bool
+
+(** [split_path s] split [s], removing some chars. *)
 val split_path : string -> string list
+
+(** A validator that checks if something looks like a name (not blank)
+    and without denominating chars like [@] and [~]. *)
 val as_name : string Yocaml.Data.validable
+
+(** [add_scheme ?scheme s] adds the [scheme] to [s] if there is no
+    scheme. *)
 val add_scheme : ?scheme:string -> string -> string
+
+(** [ltrim_path xs] remove leading empty sectin in [xs]. *)
 val ltrim_path : string list -> string list
---
+++ lib/atoms/ext/option.mli
@@ -0,0 +1,26 @@
+(** Some helpers to deal with option. *)
+
+val unit : 'a option -> unit option
+val from_bool : bool -> unit option
+val to_bool : 'a option -> bool
+
+module Syntax : sig
+  val ( let+ ) : 'a option -> ('a -> 'b) -> 'b option
+  val ( let* ) : 'a option -> ('a -> 'b option) -> 'b option
+  val ( and+ ) : 'a option -> 'b option -> ('a * 'b) option
+  val ( and* ) : 'a option -> 'b option -> ('a * 'b) option
+end
+
+module Infix : sig
+  val ( <$> ) : ('a -> 'b) -> 'a option -> 'b option
+  val ( <$ ) : 'a -> 'b option -> 'a option
+  val ( $> ) : 'a option -> 'b -> 'b option
+  val ( >>= ) : 'a option -> ('a -> 'b option) -> 'b option
+  val ( =<< ) : ('a -> 'b option) -> 'a option -> 'b option
+  val ( >|= ) : 'a option -> ('a -> 'b) -> 'b option
+  val ( <*> ) : ('a -> 'b) option -> 'a option -> 'b option
+  val ( <|> ) : 'a option -> 'a option -> 'a option
+end
+
+include module type of Infix
+include module type of Syntax
